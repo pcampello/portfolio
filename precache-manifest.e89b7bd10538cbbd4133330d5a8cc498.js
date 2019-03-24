89aa67fd46ce6278f046ba66a05e072b3eeb7cbb
PCampello <pcampello19@gmail.com>
2019-03-24 02:35:00 -0400

    Updates

---
+++ precache-manifest.e89b7bd10538cbbd4133330d5a8cc498.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "5023756ab22150d9a013",
+    "url": "/portfolio/static/js/main.6b6ac9aa.chunk.js"
+  },
+  {
+    "revision": "0a80c32931849b92cc98",
+    "url": "/portfolio/static/js/2.1c79f363.chunk.js"
+  },
+  {
+    "revision": "5023756ab22150d9a013",
+    "url": "/portfolio/static/css/main.9141f627.chunk.css"
+  },
+  {
+    "revision": "0a80c32931849b92cc98",
+    "url": "/portfolio/static/css/2.8e07419d.chunk.css"
+  },
+  {
+    "revision": "1f19bac13215d7cf44a9c1a7f53f09f7",
+    "url": "/portfolio/index.html"
+  }
+];
